d37a4c2fa9e771fafcc0e0b99b327c901d2860e4
sanjsanj <sanjsanj@hotmail.com>
2015-04-29 15:08:00 +0100

    Can find profiles

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,19 +1,21 @@
 describe('GitHub profile finder', function() {
 
-  var searchBox = element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-  var searchButton = element(by.className('btn')).click();
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
 
-  beforeEach(function(){
+  beforeEach(function() {
     browser.get('http://localhost:3000');
-  });
+  })
 
-  it('has a title', function(){
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
 
   it('finds profiles', function() {
-    searchBox;
-    searchButton;
+
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+
     expect(element(by.binding('user.login')).getText()).toEqual('spike01');
   });
 });
